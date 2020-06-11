275785a73137366961461850ef52a216c13fc6d3
Samuel Odudare <odudaresamuel@yahoo.co.uk>
2020-06-11 14:56:16 +0100

    completed part week-5

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -15,6 +15,13 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+console.log(document.querySelectorAll("p"));
+
+console.log(document.querySelector(".site-header"));
+
+console.log(document.querySelector("#jumbotron-text"));
+
+console.log(document.querySelectorAll(".primary-content p"));
 
 /*
 Task 2
@@ -23,12 +30,28 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 
+let alertButton = document.querySelector("#alertBtn");
+alertButton.addEventListener("click", visitAlert);
+
+function visitAlert(){
+    alert ("Thanks for visiting Bikes for Refugees!");
+    alertButton.style.backgroundColor="purple";
+}
+
 /*
 Task 3
 =======
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
+let changeBackgroundColor = document.querySelector("#bgrChangeBtn");
+changeBackgroundColor.addEventListener("click", changeIt);
+
+function changeIt(){
+ let bodyColor = document.querySelector("body");
+ bodyColor.style.backgroundColor = "green";
+    
+}
 
 /*
 Task 4
@@ -36,6 +59,16 @@ Task 4
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
+let textbutton = document.querySelector("#addTextBtn");
+textbutton.addEventListener("click", newPar);
+
+function newPar(){
+    let newParagraph = document.createElement("p");
+    newParagraph.innerText = "Read more below";
+
+    let theParent = document.querySelector(".buttons");
+    theParent.appendChild(newParagraph);
+}
 
 /*
 Task 5
@@ -43,6 +76,12 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+let largeLinksText = document.querySelector("#largerLinksBtn");
+largeLinksText.addEventListener("click", increaseText);
+
+function increaseText() {
+  document.body.style.fontSize = "65px";
+;}
 
 /*
 Task 6
@@ -51,3 +90,13 @@ Task 6
 Using JavaScript, create an unordered list under the "Add" button.
 When the "Add" button is pressed, get the value of the text box on its left, and add it to the list you created above.
 */
+
+let blueButton1 = document.querySelector("#blueBtn");
+blueButton1.addEventListener("click", JumbotronBckGrColor);
+
+function JumbotronBckGrColor (){
+    let btnColor = document.querySelector(".jumbotron");
+        btnColor.style.backgroundColor = "red";
+    let donBtnColor = document.querySelectorAll("..btn btn-primary btn-lrg");
+        donBtnColor.style.backgroundColor = "brown";
+}
